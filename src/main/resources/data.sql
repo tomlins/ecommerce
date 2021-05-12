022b2ec6e2d71c6cd00725a91e6a894eb8d444b1
Jason Tomlins <jason.tomlins@gmail.com>
2021-05-12 22:34:49 +1000

    Added additional record to test git

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,4 @@ insert into item (name, price, description) values ('Round Widget', 2.99, 'A wid
 insert into item (name, price, description) values ('Square Widget', 1.99, 'A widget that is square');
 insert into item (name, price, description) values ('Blue Widget', 6.99, 'A widget that is blue');
 insert into item (name, price, description) values ('Green Widget', 5.99, 'A widget that is green');
+insert into item (name, price, description) values ('Red Widget', 5.99, 'A widget that is green');
