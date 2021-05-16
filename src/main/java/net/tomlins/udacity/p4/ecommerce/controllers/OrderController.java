29c192cd3f50726a26c6be8d347cb5b3bbfa4c02
Jason Tomlins <jason.tomlins@gmail.com>
2021-05-16 20:36:04 +1000

    Added more logging to OrderController

--- src/main/java/net/tomlins/udacity/p4/ecommerce/controllers/OrderController.java
+++ src/main/java/net/tomlins/udacity/p4/ecommerce/controllers/OrderController.java
@@ -6,6 +6,8 @@ import net.tomlins.udacity.p4.ecommerce.model.persistence.User;
 import net.tomlins.udacity.p4.ecommerce.model.persistence.UserOrder;
 import net.tomlins.udacity.p4.ecommerce.model.persistence.repositories.OrderRepository;
 import net.tomlins.udacity.p4.ecommerce.model.persistence.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
-	
-	
+
+	private static final Logger LOG = LoggerFactory.getLogger(OrderController.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -30,10 +33,12 @@ public class OrderController {
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			LOG.info("submit : User trying to submit order for unknown user, {}", username);
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+		LOG.info("submit : User {} order has successfully been submitted", username);
 		return ResponseEntity.ok(order);
 	}
 	
@@ -41,8 +46,10 @@ public class OrderController {
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			LOG.info("getOrdersForUser : User {} does not exist", username);
 			return ResponseEntity.notFound().build();
 		}
+		LOG.info("getOrdersForUser : Returning orders for user {}", username);
 		return ResponseEntity.ok(orderRepository.findByUser(user));
 	}
 }
